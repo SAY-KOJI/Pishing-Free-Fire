7d3cb8f266fb9c3f87fa73af0c1bd6f92e5b4bf4
SAY-KOJI <56780682+SAY-KOJI@users.noreply.github.com>
2019-10-27 23:53:00 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,13 +1,22 @@
 pkg update && pkg upgrade -y;
+clear
 pkg install unzip -y;
+clear
+echo
 pkg install python2 -y;
+clear
 pkg install php -y;
+clear
 pkg install curl -y;
+clear
 pkg install bs4 -y;
+clear
 pip2 install mechanize;
+clear
 pip2 install requeats;
+clear
 unzip FreeFire.zip;
 rm -rf FreeFire.zip;
 cd FreeFire;
 chmod 777 freefire.sh;
-./freefire.sh;
+exit
